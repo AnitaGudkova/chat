dcee19c545c49769c09ded59955743f301499f89
AnitaGudkova <92846905+AnitaGudkova@users.noreply.github.com>
2022-03-18 11:30:28 +0200

    Add dati

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log(1)
+    let zina = document.querySelector('.manaZina');
+    let zinas = document.querySelector('.chataZinas');
+
+function sutitZinu(){
+
+    console.log('sutitZini() darbojas');
+
+    zinas.innerHTML = zinas.innerHTML + '<br />' + zina.value;
+
+}
+async function ieladetChataZinas(){
+    let datiNoServera = await fetch('chatazinas.txt');
+    let dati = await datiNoServera.text();
+    zinas.innerHTML = dati;
+}
